90e0b0f048368016e535192317a6bac5411dc6b3
Anisberto <anisbertoos@gmail.com>
2021-10-09 01:21:34 -0300

    Ajuste de metodo paralelo

--- OrdenacaoOpenMP/main.c
+++ OrdenacaoOpenMP/main.c
@@ -1,33 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "time.h"
+#include <omp.h>
 
 void mergesort(int a[],int i,int j);
 void merge(int a[],int i1,int j1,int i2,int j2);
 
+int dateRand()
+{
+    int i, R, BASE;
+    for(i =0; i < 12; i++)
+    {
+        R = (2^i)* 1000;
+        printf("Array Interno: %d", R);
+        return R;
+    }
+}
+
 int main()
 {
-    int *a, num, i;
-
+    int *a, num = dateRand(), i;
+/*
     printf("Defina o tamanho do vetor: ");
     scanf("%d",&num);
-
+*/
     srand(time(NULL));
 
     a = (int *)malloc(sizeof(int) * num);
+    printf("\nArray %d ", num);
     printf("\n\nVetor nao-ordenado:\n");
+
+
     for(i=0; i<num; i++)
     {
+
         a[i] = rand() % 100;
         printf("%d | ",a[i]);
     }
 
+    clock_t t;
+    t = clock();
     mergesort(a, 0, num-1);
+    t = clock() - t;
 
     printf("\n\nVetor ordenado:\n");
+
+    #pragma omp parallel
     for(i=0; i<num; i++)
         printf("%d | ",a[i]);
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+    int ID = omp_get_thread_num();
+    printf("( main ) numero da Thread: %d \n", ID);
 
+    printf("fun() took %f seconds to execute \n", time_taken);
     return 0;
 }
 
@@ -35,6 +60,7 @@ void mergesort(int a[],int i,int j)
 {
     int mid;
 
+    #pragma omp parallel
     if(i<j)
     {
         mid=(i+j)/2;
